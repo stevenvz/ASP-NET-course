b98229f2d78e221b691fcb9acd3902a333e31407
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-12-10 10:23:31 -0500

    added client-side validation & anti-forgery tokens to customer form

--- Vidly_auth/Vidly_auth/Models/Min18YearsIfAMember.cs
+++ Vidly_auth/Vidly_auth/Models/Min18YearsIfAMember.cs
@@ -13,7 +13,8 @@ namespace Vidly_auth.Models
         {
             var customer = (Customer) validationContext.ObjectInstance;
 
-            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            if (customer.MembershipTypeId == MembershipType.Unknown ||
+                customer.MembershipTypeId == MembershipType.PayAsYouGo)
                 return ValidationResult.Success;
 
             if (customer.Birthdate == null)
