33302ab382214fc8a8024b8d8fdd213c36c48b99
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-12-10 09:41:05 -0500

    added custom validation for new customer - checks min age 18 years for subscription accounts

---
+++ Vidly_auth/Vidly_auth/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly_auth.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer) validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthdate is required for subscription accounts (must be 18 years or older).");
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Must be 18 years or older for a subscription account.");
+        }
+    }
+}
