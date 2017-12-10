62f08d24693c19afdfcc5f9df9b8c5c6abe16bcb
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-12-09 22:30:33 -0500

    added validation to customer new/edit

--- Vidly_auth/Vidly_auth/App_Start/BundleConfig.cs
+++ Vidly_auth/Vidly_auth/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Vidly_auth
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
--- Vidly_auth/Vidly_auth/Models/Customer.cs
+++ Vidly_auth/Vidly_auth/Models/Customer.cs
@@ -12,15 +12,15 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
-        [Display(Name = "Membership Type")]
         public MembershipType MembershipType { get; set; }
 
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
